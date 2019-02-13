7c8b79503bbd710de3f63ea5fd0950f5528f44ba
AndrejTsanda <tsanda.andrej@gmail.com>
2019-02-14 00:44:24 +0300

    HTTP Status Codes.

--- src/main/java/com/tsanda/employeeApp/controller/EmployeeController.java
+++ src/main/java/com/tsanda/employeeApp/controller/EmployeeController.java
@@ -4,8 +4,12 @@ import com.tsanda.employeeApp.domain.Employee;
 import com.tsanda.employeeApp.service.EmployeeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,54 +24,128 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     @ApiOperation(value = "Get all Employees.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not found!")
+    })
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
-    public List<Employee> getAllEmployees() {
+    public ResponseEntity<List<Employee>> getAllEmployee() {
         try {
-            return this.employeeService.getAll();
+            List<Employee> employeeList = this.employeeService.getAll();
+            if (employeeList != null) {
+                return new ResponseEntity<>(employeeList, HttpStatus.OK);
+            } else {
+                log.debug("List of employees are empty!");
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
         } catch (Exception e) {
             log.error(e);
-            return null;
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @ApiOperation(value = "Save the Employee.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 204, message = "No content"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 500, message = "Error")
+    })
     @RequestMapping(value = "/employee", method = RequestMethod.POST)
-    public void saveEmployee(@RequestBody Employee employee) {
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
         try {
-            this.employeeService.saveEmployee(employee);
+            if (employee != null) {
+                this.employeeService.saveEmployee(employee);
+                return new ResponseEntity<>(employee, HttpStatus.CREATED);
+            } else {
+                log.debug("Employee is null");
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
         } catch (Exception e) {
             log.error(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @ApiOperation(value = "Delete Employee by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "No content"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 500, message = "Error")
+    })
     @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
-    public void deleteEmployee(@PathVariable("id") String id) {
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable("id") String id) {
         try {
-            this.employeeService.deleteEmployee(Integer.parseInt(id));
+            Employee employee = this.employeeService.getEmployeeById(Integer.parseInt(id));
+            if (employee != null) {
+                this.employeeService.deleteEmployee(Integer.parseInt(id));
+                return new ResponseEntity<>(employee, HttpStatus.OK);
+            } else {
+                log.debug("No object for delete was found with the given ID! ID: " + Integer.parseInt(id));
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
         } catch (Exception e) {
             log.error(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @ApiOperation(value = "Update Employee by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 204, message = "No content"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 500, message = "Error")
+    })
     @RequestMapping(value = "/employee/{id}", method = RequestMethod.PUT)
-    public void updateEmployee(@RequestBody Employee employee, @PathVariable("id") String id) {
+    public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee, @PathVariable("id") String id) {
         try {
-            this.employeeService.updateEmployee(employee, Integer.parseInt(id));
+            Employee employee1 = this.employeeService.getEmployeeById(Integer.parseInt(id));
+            if (employee1 != null) {
+                this.employeeService.updateEmployee(employee, Integer.parseInt(id));
+                return new ResponseEntity<>(employee, HttpStatus.OK);
+            } else {
+                log.debug("No object for update was found with the given ID! ID: " + Integer.parseInt(id));
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         } catch (Exception e) {
             log.error(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @ApiOperation(value = "Get Employee by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "No content"),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not found"),
+            @ApiResponse(code = 500, message = "Error")
+    })
     @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
-    public Employee getEmployeeById(@PathVariable("id") String id) {
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") String id) {
         try {
-            return this.employeeService.getEmployeeById(Integer.parseInt(id));
+            Employee employee = this.employeeService.getEmployeeById(Integer.parseInt(id));
+            if (employee != null) {
+                return new ResponseEntity<>(employee, HttpStatus.OK);
+            } else {
+                log.debug("No object was found with the given ID! ID: " + Integer.parseInt(id));
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
         } catch (Exception e) {
             log.error(e);
-            return null;
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
